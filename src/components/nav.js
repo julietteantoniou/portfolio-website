866a59ffe473300b1ba1ef79f1a894f259731e99
Julie Antonio <julieantonio38@gmail.com>
2020-04-15 00:11:30 -0400

    updated cr, added resume, emphasized gh

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,6 +5,7 @@ const Nav = () => {
         <nav>
             <a href='#about'><h2>about</h2></a>
             <a href='#projects'><h2>projects</h2></a>
+            <a href='https://drive.google.com/file/d/1XfxkDqIU__sm6N7xAaMCdmco5faMZ_Iw/view?usp=sharing' target='_blank' rel="noopener noreferrer"><h2>resume</h2></a>
             <a href='#contact'><h2>contact</h2></a>
         </nav>
     )
