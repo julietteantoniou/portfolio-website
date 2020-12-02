c95f9e2868129d3f720dacd1a35605cc07163a29
Juliette <julietteantoniou@gmail.com>
2020-12-02 17:52:13 -0500

    Update Fom.js
    Temporarily removed contact form

--- src/components/Fom.js
+++ src/components/Fom.js
@@ -39,35 +39,35 @@ if (email.message_html && email.reply_to && email.from_name){
 	return (
   	<div id='contact' className="contact">
     	<h2>Get in Touch</h2>
-        <p>You can find me on <a href='https://www.linkedin.com/in/juliette-antoniou-8633aa168' target='_blank' rel="noopener noreferrer">linkedin</a>, or send me an email using the form below! You can also email me directly at <a href="mailto:julietteantoniou@gmail.com?Subject=Let's%20Work%20Together!">julietteantoniou@gmail.com</a></p>
-        {sent ? <div className='contact-success'><h3>Thanks for reaching out! I'll get back to you as soon as possible</h3> </div> : 
-        <form>
-    	<div className='contact-top'>
-        <input 
-            name="from_name"
-            value={email.from_name}
-            placeholder='Name'
-            onChange={handleChange}
-            required
-        />
-           <input 
-            name="reply_to"
-            type='email'
-            value={email.reply_to}
-            placeholder='Email'
-            onChange={handleChange}
-            required
-        />
-        </div>
-      	<textarea
-        	name="message_html"
-        	onChange={handleChange}
-        	placeholder="Hey Juliette,"
-        	required
-        	value={email.message_html}
-      	/>
-    	<button aria-label='Send Message' onClick={(e)=>handleSubmit(e)}> SEND</button>
-  	</form>}
+        <p>You can find me on <a href='https://www.linkedin.com/in/juliette-antoniou-8633aa168' target='_blank' rel="noopener noreferrer">linkedin</a>, or send me an email at <a href="mailto:julietteantoniou@gmail.com?Subject=Let's%20Work%20Together!">julietteantoniou@gmail.com</a></p>
+//        {sent ? <div className='contact-success'><h3>Thanks for reaching out! I'll get back to you as soon as possible</h3> </div> : 
+//         <form>
+//     	<div className='contact-top'>
+//         <input 
+//             name="from_name"
+//             value={email.from_name}
+//             placeholder='Name'
+//             onChange={handleChange}
+//             required
+//         />
+//            <input 
+//             name="reply_to"
+//             type='email'
+//             value={email.reply_to}
+//             placeholder='Email'
+//             onChange={handleChange}
+//             required
+//         />
+//         </div>
+//       	<textarea
+//         	name="message_html"
+//         	onChange={handleChange}
+//         	placeholder="Hey Juliette,"
+//         	required
+//         	value={email.message_html}
+//       	/>
+//     	<button aria-label='Send Message' onClick={(e)=>handleSubmit(e)}> SEND</button>
+//   	</form>}
       </div>
 	)
   }
