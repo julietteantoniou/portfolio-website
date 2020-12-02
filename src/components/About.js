dccc6e10917be7207b1d0d456b71607e137e1e01
Juliette <julietteantoniou@gmail.com>
2020-12-02 17:41:42 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
             <div className='logo'>{logo()}</div>
             <h1>Juliette Antoniou</h1>
             <h3>Software Developer</h3>
-            <p>I've had a passion for learning how things work and creating new ones for as long as I can remember. Currently, I am a Section Lead at Lambda School, guiding a team of teaching assistants and students through Lambda School's full stack bootcamp, but am looking to bring my dedication, drive, and coding skills into a new software development role.</p>
+            <p>I've had a passion for learning how things work and creating new ones for as long as I can remember. Currently, I'm looking to bring my dedication, drive, and coding skills into a new software development role.</p>
         </section>
     )
 }
